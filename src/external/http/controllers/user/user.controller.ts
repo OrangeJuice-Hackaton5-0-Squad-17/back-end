cd4c75007c210966dcbfe11f79eb34cefd651330
mvmes2 <mvmes23@gmail.com>
2024-02-01 19:38:56 -0300

    chore: add HttpError handler to controller domain

--- src/external/http/controllers/user/user.controller.ts
+++ src/external/http/controllers/user/user.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, HttpException, HttpStatus, Post } from '@nestjs/common';
 import { CreateUserBody } from '@external/http/dtos/user/create-user-body';
 import { CreateUser } from '@app/use-cases/user/create-user';
 import { UserViewModel } from '@external/http/view-models/user-view-model';
+import { UserEmailAlreadyInUse } from '@app/use-cases/errors/user-email-already-in-use-error';
 
 @Controller('user')
 export class UserController {
@@ -9,13 +10,20 @@ export class UserController {
   @Post()
   async create(@Body() body: CreateUserBody) {
     const { name, email, password } = body;
-
-    const { user } = await this.createUser.execute({
-      name,
-      email,
-      password
-    });
-
-    return { user: UserViewModel.toHTTP(user) }
+    try {
+      const { user } = await this.createUser.execute({
+        name,
+        email,
+        password
+      });
+  
+      return { user: UserViewModel.toHTTP(user) }
+    } catch (error) {
+      if (error instanceof UserEmailAlreadyInUse) {
+        throw new HttpException('This email is already in use.', HttpStatus.BAD_REQUEST);
+      } else {
+        throw error;
+      }
+    }
   }
 }
