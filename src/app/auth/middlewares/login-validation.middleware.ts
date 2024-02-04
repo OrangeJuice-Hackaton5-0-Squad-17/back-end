5d77e45e516f116ea6beaacfe327b863cbcdc3d4
mvmes2 <mvmes23@gmail.com>
2024-02-04 18:01:30 -0300

    fix(user): fix login-validation.middleware.ts imports

--- src/app/auth/middlewares/login-validation.middleware.ts
+++ src/app/auth/middlewares/login-validation.middleware.ts
@@ -5,22 +5,14 @@ import {
   } from '@nestjs/common';
   import { NextFunction, Request, Response } from 'express';
   import { validate } from 'class-validator';
-<<<<<<< HEAD
-import { LoginRequestBody } from '../types/Login-validation-requestBody';
-=======
-import { LoginValidationRequestBody } from '../types/login-validation-requestBody';
->>>>>>> 752e9d65862bc5cfdc987ff9db4584f7fdbb044a
+import { LoginValidationRequestBody } from '../types/Login-validation-requestBody';
   
   @Injectable()
   export class LoginValidationMiddleware implements NestMiddleware {
     async use(req: Request, res: Response, next: NextFunction) {
       const body = req.body;
   
-<<<<<<< HEAD
-      const loginRequestBody = new LoginRequestBody();
-=======
       const loginRequestBody = new LoginValidationRequestBody();
->>>>>>> 752e9d65862bc5cfdc987ff9db4584f7fdbb044a
       loginRequestBody.email = body.email;
       loginRequestBody.password = body.password;
   
