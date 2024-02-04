e7fc39452602196159c3c3cc2d36b5f011a47a0f
mvmes2 <mvmes23@gmail.com>
2024-02-04 18:43:00 -0300

    att at readme

--- src/external/http/controllers/testRoute/test.controller.ts
+++ src/external/http/controllers/testRoute/test.controller.ts
@@ -1,11 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { TestService } from '@app/testRoute/testRoute.service.interface';
 import { ApiTags } from '@nestjs/swagger';
+import { IsPublic } from '@app/auth/decorators/is-public.decorator';
 @ApiTags('testRoute')
 @Controller()
 export class TestController {
   constructor(private readonly testService: TestService) {}
-
+  @IsPublic()
   @Get('test')
   getTestMessage(): string {
     return this.testService.testMessage();
